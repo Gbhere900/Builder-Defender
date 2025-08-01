0d33fe361a47bf4920402acdbefad12d6798bb95
Gbhere900 <1843123351@qq.com>
2025-08-01 10:56:09 +0800

    Make UI render after scene
    让UI显示在模型上层

--- Assets/Scripts/UI/RotateToCamera.cs
+++ Assets/Scripts/UI/RotateToCamera.cs
@@ -7,7 +7,6 @@ public class RotateToCamera : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCameraBase activeVirtualCamera;
     [SerializeField] private float cameraOffset = 1;
-    [SerializeField] private float heightOffset = 5;
 
     private void OnEnable()
     {
@@ -27,7 +26,7 @@ public class RotateToCamera : MonoBehaviour
         //    Debug.LogError("activeVirtualCamera ЮЊПе");
         //healthBar.transform.up = activeVirtualCamera.gameObject.transform.position - healthBar.transform.position;
         transform.position = gameObject.transform.parent.transform.position
-            + activeVirtualCamera.transform.up * cameraOffset + Vector3.up * heightOffset;
+            + activeVirtualCamera.transform.up * cameraOffset;
         transform.forward = activeVirtualCamera.transform.position - transform.position;
         transform.right = activeVirtualCamera.transform.up;
         // healthBar.transform.forward = - Camera.main.transform.right;
